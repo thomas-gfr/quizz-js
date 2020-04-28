7ec0dc9dc0c5ae03df2427aa87e7f15eb24bc732
ilyass200 <ajdainii@gmail.com>
2020-04-28 17:55:11 +0200

    include annim.js | qcm slider

---
+++ Includes/annim.js
@@ -0,0 +1,50 @@
+$(document).ready(function()
+{
+
+    // start qcm 
+
+    $('.btn-start').on('click',function()
+    {
+        $(this).hide(200);
+        $('.qcm-container').show(200)
+    })
+
+    // qcm slider
+    var index = 0;
+    let q_lenth = $('.qcm-box').length;
+    $('.qcm-box').eq(index).css('display','block');
+
+    $('.btn-prev').css('display','none');
+
+    $('.btn-next').on('click',function()
+    {
+        index++;
+        if(index == 1)
+        {
+            $('.btn-prev').css('display','initial');
+        }
+       $('.qcm-box').css('display','none');
+       $('.qcm-box').eq(index).css('display','block');
+       
+       if((q_lenth - 1) == index)
+        {
+            $('.btn-next').hide();
+            $('.btn-valid').show();
+            return;
+        }
+    })
+
+    $('.btn-prev').on('click',function()
+    {
+        index--;
+        $('.btn-next').css('display','block');
+        $('.btn-valid').css('display','none');
+        $('.qcm-box').css('display','none');
+        $('.qcm-box').eq(index).css('display','block');
+        if(index == 0)
+        {
+           $(this).hide(); 
+        }
+    })
+
+})
